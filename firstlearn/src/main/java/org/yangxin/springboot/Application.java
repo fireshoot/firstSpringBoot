cd9c867a2c09e92f2ae0180b11db554ec13995a7
yangxin <823721670@qq.com>
2018-12-18 10:21:12 +0800

    使用@WebServlet注解和@ServletComponentScan注解来实现绑定Servlet

--- firstlearn/src/main/java/org/yangxin/springboot/Application.java
+++ firstlearn/src/main/java/org/yangxin/springboot/Application.java
@@ -2,7 +2,11 @@ package org.yangxin.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
+import org.yangxin.springboot.controller.IndexServlet;
 
 /**
  * @author yangxin
@@ -11,11 +15,21 @@ import org.springframework.context.annotation.ImportResource;
 //标明SpringBoot的入口类
 @SpringBootApplication
 @ImportResource({"classpath:addressSpring.xml"})
+/*这个注解会在整个springBoot工程启动的时候，扫描
+整个工程的Servlet组件，并且配置到环境中能够被识别。
+*/
+@ServletComponentScan
 public class Application {
     //常规的java程序入口
     public static void main(String[] args){
         //配置SpringBoot的执行入口
         SpringApplication.run(Application.class,args);
     }
+/*    @Bean
+    public ServletRegistrationBean getBean(){
+        ServletRegistrationBean bean=new ServletRegistrationBean(new IndexServlet());
+        bean.addUrlMappings("/index.html");
+        return bean;
+    }*/
 
 }
--- firstlearn/src/main/java/org/yangxin/springboot/controller/IndexServlet.java
+++ firstlearn/src/main/java/org/yangxin/springboot/controller/IndexServlet.java
@@ -1,8 +1,27 @@
 package org.yangxin.springboot.controller;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * @author yangxin
  * @time 2018/12/18  9:55
  */
-public class IndexServlet {
+
+//servlet使用@WebServlet注解来映射URL地址
+@WebServlet(urlPatterns = "/index.html")
+public class IndexServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Index Servlet doGet() 被调用!");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
 }
